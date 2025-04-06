d0b85d38ff6102f873a200a4455a0413d0fde8af
Your Name <mohamedamgad7777@gmail.com>
2025-04-06 11:24:43 +0200

    modify amgad

--- JavaApplication6/src/javaapplication6/JavaApplication6.java
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -15,6 +15,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
        System.out.println("Hello Ibrahim");
+       System.out.println(" hello amgad");
     }
     
 }
